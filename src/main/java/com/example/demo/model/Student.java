f16d435aa912fd9ad57160dd3db5ffa40ff9aa0b
chandanrawat08 <crawat0101@gmail.com>
2025-01-25 11:34:19 +0530

    Second commit

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,5 +1,38 @@
 package com.example.demo.model;
 
 public class Student {
+	
+	private Integer sId;
+	private String name;
+	private String email;
+	
+	
+	
+	public Student(Integer sId, String name, String email) {
+		this.sId = sId;
+		this.name = name;
+		this.email = email;
+	}
+	
+	public Integer getsId() {
+		return sId;
+	}
+	public void setsId(Integer sId) {
+		this.sId = sId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
